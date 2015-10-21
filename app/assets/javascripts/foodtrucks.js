b1e5b0d62b3e95984ad83adbd31b7d2fd03a2e58
missling <ling.giang@gmail.com>
2015-10-21 10:09:24 -0700

    initial commit

--- app/assets/javascripts/foodtrucks.js
+++ app/assets/javascripts/foodtrucks.js
@@ -2,7 +2,7 @@ function initMap() {
   var myLatLng = {lat: 37.7833, lng: -122.4167};
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
+    zoom: 13,
     center: myLatLng
   });
 
@@ -14,9 +14,13 @@ function initMap() {
     var marker = new google.maps.Marker({
       position: coordinate,
       map: map,
-      title: 'Hello World!'
     });
   };
+
+  $('#name').on('click', function(){
+    console.log("hello")
+  });
+
 }
 
 
