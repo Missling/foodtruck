0c92c5687ad7ed7beefbede9c92e278a667e11d8
missling <ling.giang@gmail.com>
2015-11-21 13:18:51 -0800

    trying to implement infowindow on top of marker

--- app/assets/javascripts/foodtrucks.js
+++ app/assets/javascripts/foodtrucks.js
@@ -6,22 +6,48 @@ function initMap() {
     center: myLatLng
   });
 
-  // var trucks = gon.foodtrucks
-  
-  for (var i = 0; i < gon.foodtrucks.length; i ++) {
-    var coordinate = { lat: gon.foodtrucks[i].latitude, lng: gon.foodtrucks[i].longitude }
+  var trucks = gon.foodtrucks
 
-    var marker = new google.maps.Marker({
-      position: coordinate,
-      map: map,
-    });
+  for (var i = 0; i < trucks.length; i ++) {
+    var coordinate = { lat: parseFloat(trucks[i].latitude), lng: parseFloat(trucks[i].longitude)}
+
+      var marker = new google.maps.Marker({
+        position: coordinate,
+        map: map,
+      });
   };
+
+  // google.maps.event.addListener(marker,'click', function() {
+  //   var contentString = '<div class=specific_food><p>hello</p></div>'
+  //   var infoWindow = new google.maps.InfoWindow({
+  //       content: contentString
+  //   });
+  //   infoWindow.close();
+  //   infoWindow.open(map, this);
+  // });
+
+
+
+  google.maps.event.addListener(marker, 'click', function() {
+
+
+    debugger
+
+    var contentString = '<div id="content">'+
+        '<p>stuff</p>'+'</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    infowindow.open(map, this); //this should be marker
+  });
+
+
+}
 
   $('#name').on('click', function(){
     console.log("hello")
   });
-
-}
-
 
 
